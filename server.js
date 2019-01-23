5cba840a401f946e64e4b266f95a520b7bfddb7b
Glitch (hello-express) <none>
2019-01-23 23:39:04 +0000

    🗽🐌 Checkpoint ./server.js:2888129/561 ./images.js:2888129/70

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // init project
 const express = require('express');
 const app = express();
+const v1 = require('./v1.index');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -11,10 +12,24 @@ const app = express();
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+  response.send('test');
 });
+
+app.post('/images', function(request, response) {
+  response.send('image saved');
+});
+
+app.use('/api/v1', v1);
+app.use('/api', v1);
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
